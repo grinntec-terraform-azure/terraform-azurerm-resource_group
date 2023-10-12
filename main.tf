6d4e71cb4e14c1da9a57dc38ce5694fdffb12664
neilgri@gmail.com <neilgri@gmail.com>
2023-10-12 10:58:58 +0200

    new terraform-docs config file

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+/**
+ * # Azure Resource Group Terraform Module
+ *
+ * This Terraform module is designed to provision an Azure Resource Group.
+ *
+ * ## Features:
+ * - **Terraform Version Compatibility**: This module requires Terraform version 1.0 or newer.
+ * - **Azure Provider Version**: It uses the AzureRM provider version 3.0 or any newer version below 4.0.
+ * - **Tagging**: Allows tagging of resources with `app` and `env` tags.
+ * - **Resource Group**: Provisions an Azure Resource Group with a name pattern of `rg-{app_name}-{environment}`.
+ * - **Location Validation**: Ensures that the provided location is one of 'westeurope', 'eastus', or 'southeastasia'.
+ * - **Environment Validation**: Validates that the environment is either 'dev', 'test', or 'prod'.
+ * - **App Name Validation**: Ensures the app name starts with a letter, ends with a letter or number, and is between 1 and 90 characters.
+ * - **Outputs**: Outputs the name and location of the provisioned resource group.
+ *
+ * ## Usage:
+ * To use this module, provide values for the required variables: `location`, `environment`, and `app_name`.
+ * Once applied, the module will provision the specified Azure Resource Group and output its name and location.
+ *
+ * Note: Always ensure you're using the correct Terraform and provider versions before applying.
+ */
+
+
 ############################################################
 # TERRAFORM CONFIGURATION
 ############################################################
